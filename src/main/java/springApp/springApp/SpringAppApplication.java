fda3dce0199b5906e36d6f2d931dcaa0b7b45f36
axelmengue1 <axelmengue1@gmail.com>
2022-12-15 00:36:59 +0100

    554

--- src/main/java/springApp/springApp/SpringAppApplication.java
+++ src/main/java/springApp/springApp/SpringAppApplication.java
@@ -2,12 +2,20 @@ package springApp.springApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class SpringAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringAppApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	String getAll(){
+		return "Hello Everybody";
 	}
 
 }
